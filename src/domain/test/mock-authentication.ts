5fd1399e96abbb379b9c10a42f5d2df33f298695
Decante <gustavocamposantunes@gmail.com>
2024-10-30 16:12:04 -0300

    feat: ensure RemoteAuthentication returns an Account on success

--- src/domain/test/mock-authentication.ts
+++
@@ -1,9 +0,0 @@
-import { faker } from "@faker-js/faker"
-
-import { AuthenticationParams } from "../usecases/authentication"
-
-export const mockAuthentication = (): AuthenticationParams => ({
-    email: faker.internet.email(),
-    password: faker.internet.password()    
-})
-
