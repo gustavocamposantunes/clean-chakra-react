c34b26a6c69146a8be39fed867ab1519eec87141
Decante <gustavocamposantunes@gmail.com>
2024-10-31 00:29:30 -0300

    feat: ensure AxiosHttpClient returns correct values

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -1,11 +1,16 @@
 import axios from "axios";
 
-import { HttpPostParams } from "@/data/protocols/http";
+import { HttpPostClient, HttpPostParams, HttpResponse } from "@/data/protocols/http";
 
 
 
-export class AxiosHttpClient<T> {
-  async post (params: HttpPostParams<T>): Promise<void> {
-    await axios.post(params.url, params.body)
+export class AxiosHttpClient<T, R> implements HttpPostClient<T, R> {
+  async post (params: HttpPostParams<T>): Promise<HttpResponse<R>> {
+    const httpResponse = await axios.post(params.url, params.body)
+
+    return {
+      statusCode: httpResponse.status,
+      body: httpResponse.data
+    }
   }
 }
