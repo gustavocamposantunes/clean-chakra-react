02d482a6086e4a9e4757819bc8053be1d24d4c85
Decante <gustavocamposantunes@gmail.com>
2024-11-01 23:24:48 -0300

    test: ensure LoginPage starts with submit button disabled

--- src/presentation/pages/login-page.spec.tsx
+++ src/presentation/pages/login-page.spec.tsx
@@ -6,11 +6,11 @@ import { render, screen } from "../test/test-utils"
 import { LoginPage } from "./login-page"
 
 describe("LoginPage", () => {
-  it("Should not render spinner and error on start", () => {
+  it("Should start with initial state", () => {
     render(<LoginPage />)
-
     const error = screen.getByTestId("error-wrap")
-
     expect(error.childElementCount).toBe(0)
+    const submitButton = screen.getByTestId("submit-button") as HTMLButtonElement
+    expect(submitButton.disabled).toBeTruthy()
   })
 })
