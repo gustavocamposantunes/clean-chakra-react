fc2b898c4ea7beca48d247592d01b97672701231
Decante <gustavocamposantunes@gmail.com>
2024-11-02 14:48:18 -0300

    test: ensure LoginPage calls validation with correct email

--- src/presentation/pages/login-page.spec.tsx
+++ src/presentation/pages/login-page.spec.tsx
@@ -1,13 +1,41 @@
-import { describe, expect, it } from "vitest"
+import { afterEach, describe, expect, it } from "vitest"
 
-import { render, screen } from "../test/test-utils"
-
+import { render, screen, fireEvent, cleanup } from "../test/test-utils"
 
 import { LoginPage } from "./login-page"
 
+import { Validation } from "@/presentation/protocols/validation"
+
+
+
+type SutTypes = {
+  validationSpy: ValidationSpy
+}
+
+class ValidationSpy implements Validation {
+  errorMessage: string
+  input: object
+  validate(input: object): string {
+    this.input = input
+    return this.errorMessage
+  }
+}
+
+
+const makeSut = (): SutTypes => {    
+  const validationSpy = new ValidationSpy()
+  render(<LoginPage validation={validationSpy} />)
+
+  return {
+    validationSpy
+  }
+}
+
 describe("LoginPage", () => {
+  afterEach(cleanup)
+
   it("Should start with initial state", () => {
-    render(<LoginPage />)
+    makeSut()
     const error = screen.getByTestId("error-wrap")
     expect(error.childElementCount).toBe(0)
     const submitButton = screen.getByTestId("submit-button") as HTMLButtonElement
@@ -18,5 +46,14 @@ describe("LoginPage", () => {
     const passwordStatus = screen.getByTestId("password-status")
     expect(passwordStatus.title).toBe("Campo obrigatório")
     expect(passwordStatus.textContent).toBe("🔴")
+  })
+
+  it("should call Validation with correct email", () => {
+    const { validationSpy } = makeSut()
+    const emailInput = screen.getByTestId("email")
+    fireEvent.change(emailInput, { target: { value: "any_email" } })
+    expect(validationSpy.input).toEqual({
+      email: "any_email"
+    })
   })
 })
