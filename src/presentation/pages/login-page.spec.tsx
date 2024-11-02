6f948f68e085ff4a0671f2cdac3dea5221e6d58d
Decante <gustavocamposantunes@gmail.com>
2024-11-02 15:34:58 -0300

    refactor: move ValidationSpy to its own file

--- src/presentation/pages/login-page.spec.tsx
+++ src/presentation/pages/login-page.spec.tsx
@@ -4,23 +4,13 @@ import { render, screen, fireEvent, cleanup } from "../test/test-utils"
 
 import { LoginPage } from "./login-page"
 
-import { Validation } from "@/presentation/protocols/validation"
+import { ValidationSpy } from "@/presentation/test"
+
 
 
 
 type SutTypes = {
   validationSpy: ValidationSpy
-}
-
-class ValidationSpy implements Validation {
-  errorMessage: string
-  fieldName: string
-  fieldValue: string
-  validate(fieldName: string, fieldValue: string): string {
-    this.fieldName = fieldName
-    this.fieldValue = fieldValue
-    return this.errorMessage
-  }
 }
 
 
