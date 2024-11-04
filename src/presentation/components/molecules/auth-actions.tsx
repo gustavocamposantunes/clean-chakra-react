10572d494766cf2d7dc5cbb8eb7f76fa231c6819
Decante <gustavocamposantunes@gmail.com>
2024-11-04 13:06:16 -0300

    feat: ensure Login disable submit button only if form is invalid

--- src/presentation/components/molecules/auth-actions.tsx
+++ src/presentation/components/molecules/auth-actions.tsx
@@ -1,28 +1,33 @@
 import { Flex, Link } from "@chakra-ui/react"
-import React from "react"
+import React, { useContext } from "react"
 
 import { CustomButton, FormStatus } from "../atoms"
+
+import Context from "@/presentation/contexts/form/form-context"
 
 interface IAuthActions {
   onLogin?(): void
 }
 
-export const AuthActions: React.FC<IAuthActions> = ({ onLogin }) => (
-  <Flex 
-    pt={3}
-    direction="column" 
-    align="center"
-    spaceY={2}
-  >
-    <CustomButton 
-      type="submit"
-      onClick={onLogin}
-      data-testid="submit-button"
-      disabled
+export const AuthActions: React.FC<IAuthActions> = ({ onLogin }) => {
+  const { state } = useContext(Context)
+  return (
+    <Flex 
+      pt={3}
+      direction="column" 
+      align="center"
+      spaceY={2}
     >
-      Entrar
-    </CustomButton>
-    <Link textAlign="center">Criar conta</Link>
-    <FormStatus />
-  </Flex>
-)
+      <CustomButton 
+        type="submit"
+        onClick={onLogin}
+        data-testid="submit-button"
+        disabled={!!state.emailError || !!state.passwordError}
+      >
+        Entrar
+      </CustomButton>
+      <Link textAlign="center">Criar conta</Link>
+      <FormStatus />
+    </Flex>
+  )
+}
