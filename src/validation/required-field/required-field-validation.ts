dddae1946e1a321beff54960226ccc19df45c820
Decante <gustavocamposantunes@gmail.com>
2024-11-06 15:23:40 -0300

    feat: ensure RequiredFieldValidation returns error if field is empty

---
+++ src/validation/required-field/required-field-validation.ts
@@ -0,0 +1,9 @@
+import { RequiredFieldError } from "@/validation/errors";
+import { FieldValidation } from "@/validation/protocols";
+
+export class RequiredFieldValidation implements FieldValidation {
+  constructor (readonly field: string) {}
+  validate(value: string): Error {
+    return value ? null : new RequiredFieldError()
+  }
+}
