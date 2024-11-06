1bd4e3f981ef6184c19dcf6a104151598750be92
Decante <gustavocamposantunes@gmail.com>
2024-11-06 15:27:22 -0300

    test: ensure RequiredFieldValidation returns falsy if field is not empty

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -1,3 +1,4 @@
+import { faker } from "@faker-js/faker"
 import { describe, expect, it } from "vitest";
 
 import { RequiredFieldValidation } from "./required-field-validation";
@@ -9,5 +10,11 @@ describe("RequiredFieldValidation", () => {
     const sut = new RequiredFieldValidation("email")    
     const error = sut.validate("")
     expect(error).toEqual(new RequiredFieldError()) 
+  })
+
+  it("Should return false if field is not empty", () => {
+    const sut = new RequiredFieldValidation("email")    
+    const error = sut.validate(faker.word.words())
+    expect(error).toBeFalsy() 
   })
 })
