f467554d205c4099101b9c4eaeb00cc14084e798
Decante <gustavocamposantunes@gmail.com>
2024-11-07 14:48:07 -0300

    test: ensure ValidationBuilder has an email helper

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -1,10 +1,15 @@
 import { describe, expect, it } from "vitest";
 
-import { RequiredFieldValidation, ValidationBuilder as sut } from "@/validation/validators";
+import { EmailValidation, RequiredFieldValidation, ValidationBuilder as sut } from "@/validation/validators";
 
 describe("ValidationBuilder", () => {
   it("Should return RequiredFieldValidation", () => {
     const validations = sut.field("any_field").required().build()
     expect(validations).toEqual([new RequiredFieldValidation("any_field")])
+  })
+
+  it("Should return EmailValidation", () => {
+    const validations = sut.field("any_field").email().build()
+    expect(validations).toEqual([new EmailValidation("any_field")])
   })
 })
