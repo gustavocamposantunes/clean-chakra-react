732e06d5bf25dd3b5da0a7bb8f46b9bec3619105
Decante <gustavocamposantunes@gmail.com>
2024-11-07 17:44:18 -0300

    chore: remove blank lines from file

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -2,8 +2,6 @@ import axios from "axios";
 
 import { HttpPostClient, HttpPostParams, HttpResponse } from "@/data/protocols/http";
 
-
-
 export class AxiosHttpClient<T, R> implements HttpPostClient<T, R> {
   async post (params: HttpPostParams<T>): Promise<HttpResponse<R>> {
     const httpResponse = await axios.post(params.url, params.body)
