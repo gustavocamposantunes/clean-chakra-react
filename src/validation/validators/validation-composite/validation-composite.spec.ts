56d33db07ce3d260dab8d74c017127b39c83e6da
Decante <gustavocamposantunes@gmail.com>
2024-11-06 23:21:03 -0300

    test: ensure ValidationComposite returns error if any validation fails

---
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -0,0 +1,19 @@
+import { describe, expect, it } from "vitest"
+
+import { ValidationComposite } from "./validation-composite"
+
+import { FieldValidationSpy } from "@/validation/test/mock-field-validation"
+
+describe("ValidationComposite", () => {
+  it("Should return error if any validation fails", () => {
+    const fieldValidationSpy = new FieldValidationSpy("any_field")
+    const fieldValidationSpy2 = new FieldValidationSpy("any_field")
+    fieldValidationSpy2.error = new Error("any_error_message")
+    const sut = new ValidationComposite([
+      fieldValidationSpy,
+      fieldValidationSpy2
+    ])
+    const error = sut.validate("any_field", "any_value")
+    expect(error).toBe("any_error_message")
+  })
+})
