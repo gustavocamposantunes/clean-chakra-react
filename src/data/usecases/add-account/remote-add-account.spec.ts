ecef97e0a5d69dd33ee236dbe9949824af126666
Decante <gustavocamposantunes@gmail.com>
2024-11-14 16:33:35 -0300

    test: ensure RemoteAddAccount throws UnexpectedError if HttpPostClient returns 500

--- src/data/usecases/add-account/remote-add-account.spec.ts
+++ src/data/usecases/add-account/remote-add-account.spec.ts
@@ -71,4 +71,14 @@ describe("RemoteAddAccount", () => {
 
     await expect(promise).rejects.toThrow(new UnexpectedError())
   })
+
+  it("Should throw UnexpectedError if HttpPostClient returns 500", async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.serverError
+    }
+    const promise = sut.add(mockAddAccountParams())
+
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
 })
