f1692fb843bb88de0465d25d123d5abc4301719f
Decante <gustavocamposantunes@gmail.com>
2024-11-14 15:59:40 -0300

    fix: correcting accessToken spelling

--- src/domain/test/mock-account.ts
+++ src/domain/test/mock-account.ts
@@ -10,5 +10,5 @@ export const mockAuthentication = (): AuthenticationParams => ({
 })
 
 export const mockAccountModel = (): AccountModel => ({
-    accesToken: faker.string.uuid()
+    accessToken: faker.string.uuid()
 })
