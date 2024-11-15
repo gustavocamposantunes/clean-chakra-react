20deb992881bd93374ec98d603d292f3c6093567
Decante <gustavocamposantunes@gmail.com>
2024-11-15 19:01:14 -0300

    feat: ensure Signup change name status to error if validation fails

--- src/presentation/pages/signup-page.tsx
+++ src/presentation/pages/signup-page.tsx
@@ -1,21 +1,35 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 
 import { SignUpTemplate } from "@/presentation/components/templates"
 import Context from "@/presentation/contexts/form/form-context"
+import { Validation } from "@/presentation/protocols/validation"
 
 
-export const SignUpPage: React.FC = () => {
-  const [state] = useState({
+type Props = {
+  validation: Validation
+}
+export const SignUpPage: React.FC<Props> = ({
+  validation
+}) => {
+  const [state, setState] = useState({
     isLoading: false,
-    nameError: "Campo obrigatório",
+    name: "",
+    nameError: "",
     emailError: "Campo obrigatório",
     passwordError: "Campo obrigatório",
     passwordConfirmationError: "Campo obrigatório",
     mainError: ""
   })
 
+  useEffect(() => {
+    setState({
+      ...state,
+      nameError: validation.validate('name', state.name)
+    })    
+  }, [state.name])
+
   return (
-    <Context.Provider value={{ state }}>
+    <Context.Provider value={{ state, setState }}>
       <SignUpTemplate />
     </Context.Provider>
   )
