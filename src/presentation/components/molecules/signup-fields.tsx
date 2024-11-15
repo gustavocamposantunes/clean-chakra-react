63ac8bfca58d9f9700d5f42477ca897541798c78
Decante <gustavocamposantunes@gmail.com>
2024-11-15 16:40:32 -0300

    fix: correct spelling for passwordConfirmation

--- src/presentation/components/molecules/signup-fields.tsx
+++ src/presentation/components/molecules/signup-fields.tsx
@@ -5,6 +5,6 @@ export const SignupFields = () => (
     <CustomInput type="text" name="name" placeholder="Digite seu nome" />
     <CustomInput type="email" name="email" placeholder="Digite seu e-mail" />
     <CustomInput type="password" name="password" placeholder="Digite sua senha" />
-    <CustomInput type="password" name="passwordConfirmaition" placeholder="Repita sua senha" />
+    <CustomInput type="password" name="passwordConfirmation" placeholder="Repita sua senha" />
   </>
 )
