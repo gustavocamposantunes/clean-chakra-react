7ca49342247fa6314b549bcc8fb5ad2a81774a2a
Decante <gustavocamposantunes@gmail.com>
2024-11-15 18:28:33 -0300

    feat: ensure Signup starts with correct initial state

--- src/presentation/pages/signup-page.tsx
+++ src/presentation/pages/signup-page.tsx
@@ -1,11 +1,22 @@
-import React from "react"
+import React, { useState } from "react"
 
 import { SignUpTemplate } from "@/presentation/components/templates"
 import Context from "@/presentation/contexts/form/form-context"
 
 
-export const SignUpPage: React.FC = () => (
-  <Context.Provider value={{ state: {} }}>
-    <SignUpTemplate />
-  </Context.Provider>
-)
+export const SignUpPage: React.FC = () => {
+  const [state] = useState({
+    isLoading: false,
+    nameError: "Campo obrigatório",
+    emailError: "Campo obrigatório",
+    passwordError: "Campo obrigatório",
+    passwordConfirmationError: "Campo obrigatório",
+    mainError: ""
+  })
+
+  return (
+    <Context.Provider value={{ state }}>
+      <SignUpTemplate />
+    </Context.Provider>
+  )
+}
