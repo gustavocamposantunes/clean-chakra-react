b2f9809399e5482d7c1ce1968cee57966b7ecb27
Decante <gustavocamposantunes@gmail.com>
2024-11-17 23:00:10 -0300

    chore: ignore storybook-repo from eslit config

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import pluginImport from 'eslint-plugin-import'
 
 export default tseslint.config(
-  { ignores: ['dist', 'coverage', 'coverage-repo', 'storybook-static'] },
+  { ignores: ['dist', 'coverage', 'coverage-repo','storybook-repo', 'storybook-static'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
