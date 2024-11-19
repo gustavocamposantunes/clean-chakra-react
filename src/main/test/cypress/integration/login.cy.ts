31686c11d62a17b6585605fcf3b661dcc0c55e7a
Decante <gustavocamposantunes@gmail.com>
2024-11-19 16:07:39 -0300

    test: ensure login begin with correct initial state

--- src/main/test/cypress/integration/login.cy.ts
+++ src/main/test/cypress/integration/login.cy.ts
@@ -1,5 +1,14 @@
 describe("Login", () => {
-  it("Shuld load with correct initial state", () => {
+  beforeEach(() => {
     cy.visit("login")
+  })
+  it("Shuld load with correct initial state", () => {
+    cy.getByTestId("email-status")
+      .should("have.attr", "title", "Campo obrigatório")
+      .should("contain.text", "🔴")
+    cy.getByTestId("password-status")
+      .should("have.attr", "title", "Campo obrigatório")
+      .should("contain.text", "🔴")
+    cy.getByTestId("error-wrap").should("not.have.descendants")
   })
 })
