404b75a25076f9b8095e71284d47a0eb18326a9a
Decante <gustavocamposantunes@gmail.com>
2024-11-19 20:50:09 -0300

    test: ensure Login present error message if invalid credentials are provided

--- src/main/test/cypress/integration/login.cy.ts
+++ src/main/test/cypress/integration/login.cy.ts
@@ -1,5 +1,6 @@
 import { faker } from "@faker-js/faker"
 
+const baseUrl = Cypress.config().baseUrl
 describe("Login", () => {
   beforeEach(() => {
     cy.visit("login")
@@ -40,5 +41,23 @@ describe("Login", () => {
       .should("contain.text", "🟢")
     cy.getByTestId("submit-button").should("not.have.attr", "disabled")
     cy.getByTestId("error-wrap").should("not.have.descendants")
+  })
+
+  it("Shuld present error if invalid credentials are invalid", () => {
+    cy.getByTestId("email").type(faker.internet.email())
+    cy.getByTestId("email-status")
+      .should("have.attr", "title", "Tudo certo")
+      .should("contain.text", "🟢")
+    cy.getByTestId("password").type(faker.string.alphanumeric(6))
+    cy.getByTestId("password-status")
+      .should("have.attr", "title", "Tudo certo")
+      .should("contain.text", "🟢")
+    cy.getByTestId("submit-button").click()
+    cy.getByTestId("error-wrap")
+      .getByTestId("spinner").should("exist")
+      .getByTestId("main-error").should("not.exist")
+      .getByTestId("spinner").should("not.exist")
+      .getByTestId("main-error").should("contain.text", "Credenciais inválidas")
+    cy.url().should("eq", `${baseUrl}/login`)
   })
 })
